3b83d93d043aded900768518026cadb6ea5acc38
Karth1kn <75937944+Karth1kn@users.noreply.github.com>
2024-01-27 12:03:30 +0530

    Update D3_FRAME.py. Banded matrix solution added. replaced LU decomposition

--- D3_FRAME.py
+++ D3_FRAME.py
@@ -2,13 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 from mpl_toolkits import mplot3d
-import math
+import scipy.linalg as slin
+import math 
 import csv
 import time
 import os
 import sys
-
-#from Utility_Functions import matrix_inverter, matrix_multiplier, global_stiff_mat
+import scipy
+from scipy.linalg import solveh_banded
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 coord_path = os.path.join(script_dir, 'coord.csv')
@@ -21,9 +22,8 @@ disp_path = os.path.join(script_dir,'displacements.csv')
 rot_path = os.path.join(script_dir,'rotates.csv')
 
 l1 = time.time()
-fig = plt.figure(figsize=(10,10))
-ax = fig.add_subplot(111, projection= '3d')
-#fig,ax = plt.subplots(figsize = (10,10))
+
+
 with open(coord_path, 'r') as file:
     try:
         val_list = []
@@ -41,7 +41,6 @@ with open(coord_path, 'r') as file:
                 x_ele = [val_array[k[0],0],val_array[k[1],0]]
                 y_ele = [val_array[k[0],1],val_array[k[1],1]]
                 z_ele = [val_array[k[0],2],val_array[k[1],2]]
-                #ax.plot3D(x_ele, y_ele, z_ele, 'blue')
 
     except Exception:
         print('error')
@@ -51,7 +50,6 @@ m= con_array
 
 #### BOUNDARY CONDITIIONS
 with open(bc_path, 'r') as bc:
-        #bc_lst =[1 for i in range(len(val_list*3))]
         bc_lst = []
         c = 0
         for c1 in csv.reader(bc):
@@ -132,7 +130,6 @@ if len(jt)!=0:
     elem = [int(k) for k in jt[0][1:-1]]
 else : elem=[]
 
-#np.set_printoptions(precision=2)
 trans_mats = []
 def lsm(element):
     al = 0
@@ -157,7 +154,7 @@ def lsm(element):
 
     def filler(lst,start_row, end_row,a,b):
         for i, val in enumerate(lst):
-            stiff[start_row+a*i, end_row+b*i] = val
+            stiff[start_row+a*i, end_row+b*i] = val #CSR STORAGE
 
     filler(main_off_diag,0,6,1,1)
     filler(main_off_diag,6,0,1,1)
@@ -195,15 +192,11 @@ def lsm(element):
     trans = np.zeros((12,12))
     for i in range(4):
         trans[3*i:3*i+3 , 3*i:3*i+3] = rotate@delta
-    #print(li,mi,ni, di)    
-    #print(delta)
-
-    local_stiff = np.transpose(trans)@stiff@trans
-    return local_stiff
+    
+    return np.transpose(trans)@stiff@trans
     #print(local_stiff)
 
 def global_stiff_mat(cord_list,dof):
-    #dof = for element
     ran = int((dof/2))
     matrix= np.zeros([len(cord)*ran,len(cord)*ran])
     global e_len
@@ -214,14 +207,10 @@ def global_stiff_mat(cord_list,dof):
         t2 = np.array([[r,s]for r in range(0,ran+1,ran) for s in range(0,ran+1,ran)])
         for j in range(4):
            matrix[6*t1[j,0]:6*t1[j,0]+6 , 6*t1[j,1]:6*t1[j,1]+6] += k[t2[j,0]:t2[j,0]+6 , t2[j,1]:t2[j,1]+6]
-
-
-
         e_len.append(ln(*i))
     np.set_printoptions(precision=2)
     return matrix
 
-#print(global_stiff_mat(m,12))
 def force_matrix(node,force):
     f_matrix= np.zeros([6*len(cord),1])
     k = 6*node
@@ -242,33 +231,70 @@ for i,wt in enumerate(m):
     force_mat[6*wt[1]+2] += -weight
 
 def boundary_conditions(matrix,bc_list):
-    matrix= np.delete(matrix,bc_list,0)
+    #matrix= np.delete(matrix,bc_list,0)
+    matrix[bc_list , :] =0
+
     if len(matrix[0])!=1:
-        matrix= np.delete(matrix,bc_list,1)
+        #matrix= np.delete(matrix,bc_list,1)
+        matrix[: , bc_list] =0
+        for i in bc_list:
+            matrix[i,i]=1
     np.set_printoptions(precision=2)
     return matrix
 
-
 global_stiff_matrix= global_stiff_mat(m,12)
-#print(global_stiff_matrix)
+
 #APPLYING BOUNDARY CONDITIONS
 bc_stiff_mat= boundary_conditions(global_stiff_matrix,bc_lst)
-#print(bc_stiff_mat)
+#print(len(bc_stiff_mat))
 bc_force_matrix= boundary_conditions(force_mat,bc_lst)
 
+def rnge():
+    t5 = time.time()
+    max_arr = [abs(i[0]-i[1]) for i in con_array ]
+    t6 = time.time()
+    return max(max_arr)*6+6
 
+def sparser():
+    oy = []
+    for q in range(len(cord)):
+        for w in range(len(cord)):
+            if not np.array_equal(global_stiff_matrix[6*q:6*q+6 , 6*w:6*w+6] , np.zeros([6,6])):
+                oy.append([w,q])
+
+    print(f'Sparsity = {1- (len(oy)/len(cord)**2)}')
+    plt.scatter(np.array(oy)[:,0] , np.array(oy)[:,1],s=1)
+    jk = 0
+    plt.xlim(0-jk,len(cord)+jk)
+    plt.ylim(0-jk,len(cord)+jk)
+    plt.gca().invert_yaxis()
+    plt.show()
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        function_name = sys.argv[1]
+        if function_name == "sparser":
+            sparser()
+t5 = time.time()
 def main_sequence():
-    bc_stiff_mat_inv= np.linalg.inv(bc_stiff_mat) 
-    #MULTIPLYING INVERSE TO FORCE MATRIX
-    nodal_displacement= bc_stiff_mat_inv@bc_force_matrix
+    global t3
+    t3 = time.time()
+    bandwidth = rnge()         
+    diagonals = np.array([np.append(np.diag(bc_stiff_mat, -i), np.zeros([1,i])[0]) for i in range(bandwidth)  ])
+    nodal_displacement = solveh_banded(diagonals, bc_force_matrix, lower= True)
 
-    for i in bc_lst:
-        nodal_displacement= np.insert(nodal_displacement,i,0)
-
+    global t4
+    t4 = time.time()
+    # Uncomment for solution using LU decomposition
+    #nodal_displacement = slin.solve(bc_stiff_mat,bc_force_matrix )
+    global t5
+    t5 = time.time()
+    nodal_displacement = [item for sublist in nodal_displacement for item in sublist]
     with open(disp_path,'w') as disp:
         writer = csv.writer(disp, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(nodal_displacement)
 
+main_sequence()
 l2 = time.time()
 if __name__ == "__main__":
     # Check if the script is run as the main program
@@ -276,11 +302,13 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         function_name = sys.argv[1]
         if function_name == "main_sequence":
+            t1 = time.time()
             main_sequence()
-            #print(l2-l1)
-            
-
-#main_sequence()
+            t2 = time.time()
+            print(f'Solution time only Thomas: {t4-t3}')
+            print(f'Solution time only LU: {t5-t4}')
+            #print('Solution time plus write: ', t2-t1, '\n')
+            #print(f'Total time: {(l2-l1) + (t2-t1)}', '\n')
 
 
 nodal_disp = []
@@ -294,17 +322,45 @@ nodal_disp1 = nodal_disp.copy()
 
 
 shape_nodal_disp= nodal_disp1.reshape(len(cord),6)
-
-#react = global_stiff_matrix@nodal_disp -force_mat[0:,0]
 for i,val in enumerate(cord):
     shape_nodal_disp[i,0:3]+=val
 cord = shape_nodal_disp[:,0:3]
-#print(shape_nodal_disp[:,:3])
 def_e_len= [ln(*i) for i in m]
 
-#print(np.array(trans_mats))
+def transfmat(element):
+    li,mi,ni = cosx(*element), cosy(*element), cosz(*element)
+
+
+    if mi==0 and ni==0 :
+        if li<0:
+            delta = np.array([[-1,0,0],
+                            [0,-1,0],
+                            [0,0,1]])
+            
+        elif li>0:
+            delta = np.array([[1,0,0],
+                            [0,1,0],
+                            [0,0,1]])  
+    else:
+        if li>0:
+            li= -li
+            mi = -mi
+            ni = -ni
+        di = (mi**2 + ni**2)**0.5
+        delta = np.array([[li,        mi,        ni],      
+                            [0 ,    -ni/di,     mi/di],
+                            [di, -mi*li/di, -ni*li/di]])
+
+        
+        dlta = np.array([[li,mi,di],
+                            [-mi/di,li/di,0],
+                            [-li*ni/di,-mi*ni/di,di]])
+
+    trans_mats.append(delta)
+
 loc_disp = []
 for ind,val in enumerate(m):
+    #transfmat(val)
     trans = np.zeros((12,12))
     o = np.array(trans_mats)[ind]
     for l in range(4):
@@ -316,7 +372,6 @@ for ind,val in enumerate(m):
 
     np.set_printoptions(precision=2)
 loc_disp = np.array(loc_disp)
-#print(nodal_disp.reshape(len(cord),6))
 
 axial_stress = []
 zb_strs  =[]
@@ -340,12 +395,9 @@ for a,ij in enumerate(loc_disp):
     yb_strs.append(avg2*(do/2)*youngs_modulus/fac)
 
     x_tors.append(((ij[9] - ij[3])*(do/2)/lt)*shear_modulus/fac)
-
-
-#print(axial_stress)
-
-#print(x_tors)
 def color_plotter(coordinate):
+    fig = plt.figure(figsize=(10,10))
+    ax = fig.add_subplot(111, projection= '3d')
     global t3
     t3 = time.time()
     with open(option_path,'r') as file:
@@ -359,10 +411,10 @@ def color_plotter(coordinate):
             elif i[0] == 'torsion':
                 value = x_tors
             for j,k in enumerate(m):
-                k=[coordinate[k[0]],coordinate[k[1]]]
-                c = [k[0][0],k[1][0]]
-                d = [k[0][1],k[1][1]]
-                e = [k[0][2],k[1][2]]
+                ks = np.array([coordinate[k[0]],coordinate[k[1]]])
+                c = np.array([ks[0,0],ks[1,0]])
+                d = np.array([ks[0,1],ks[1,1]])
+                e = np.array([ks[0,2],ks[1,2]])
 
                 fos= value[j]/yield_stress
                 #print(fos)
@@ -380,12 +432,11 @@ def color_plotter(coordinate):
 
     ax.set_facecolor('black')
     ax.grid(visible=False)
-    sc = 2
-    x = 3
-    plt.xlim(-x-sc,x+sc)
-    plt.ylim(-x-sc,x+sc)
+    sc = 0
+    x = 5
+    ax.set_xlim(-x-sc,x+sc)
+    ax.set_ylim(-x-sc,x+sc)
     ax.set_zlim(-x-sc,x+sc)
-    #ax.set_box_aspect([1, 1, 1])
     global t4
     t4 = time.time()
     plt.show()
@@ -399,6 +450,9 @@ if __name__ == "__main__":
         function_name = sys.argv[1]
         if function_name == "ploter":
             ploter()
-            print(t4-t3)
+            print(f'Plotting time: {t4-t3}')
+
+
+            
 
 
